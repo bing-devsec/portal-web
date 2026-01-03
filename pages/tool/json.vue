f55c5684aa8ed988b34a0ef7449e32258c352b8e
binary <binary>
2026-01-03 13:53:27 +0800

    1.3

--- pages/tool/json.vue
+++ pages/tool/json.vue
@@ -1,3 +1,13 @@
 <template>
-    <JsonTool />
+    <client-only>
+        <AsyncJsonTool />
+    </client-only>
 </template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const AsyncJsonTool = defineAsyncComponent(
+    () => import("~/components/JsonTool/JsonTool.client.vue")
+);
+</script>
